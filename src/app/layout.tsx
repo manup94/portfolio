685a3edca7a17a83ecac0c107b4f763f283509a4
ManuelPerezPrado <manuel.perez@asafedigital.com>
2024-05-24 19:24:36 +0200

    Add analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import { GeistSans } from "geist/font/sans";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Manuel Pérez - Portfolio",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={`bg-pattern ${GeistSans.className} max-w-7xl mx-auto`}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
