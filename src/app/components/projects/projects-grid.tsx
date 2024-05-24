805e111316a9b8824fe6fc16fc0e860cf85ebb1b
ManuelPerezPrado <manuel.perez@asafedigital.com>
2024-05-24 14:28:22 +0200

    changes

--- src/app/components/projects/projects-grid.tsx
+++ src/app/components/projects/projects-grid.tsx
@@ -4,12 +4,10 @@ import ProjectItem from "./projects-item";
 const ProjectsGrid = () => {
   return (
     <section id="proyectos" className="py-12 md:py-24 lg:py-32">
-      <div className="">
-        <div className="grid lg:gap-6 gap-12 lg:grid-cols-2 grid-cols-1 lg:p-0 p-4">
-          {projects.map((project) => {
-            return <ProjectItem key={project.id} project={project} />;
-          })}
-        </div>
+      <div className="grid lg:gap-6 gap-12 lg:grid-cols-2 grid-cols-1 lg:p-0 p-4">
+        {projects.map((project) => {
+          return <ProjectItem key={project.id} project={project} />;
+        })}
       </div>
     </section>
   );
