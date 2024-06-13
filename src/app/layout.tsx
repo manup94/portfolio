24e243f2562655e2ab3df3160ca8ec6f0a94f497
ManuelPerezPrado <manuel.perez@asafedigital.com>
2024-06-13 19:19:43 +0200

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,28 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from "./components/navbar";
-import { GeistSans } from "geist/font/sans";
-import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next"
+import "./globals.css"
+import Navbar from "./components/navbar"
+import { GeistSans } from "geist/font/sans"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Manuel Pérez - Portfolio",
   description: "Frontend Developer portfolio",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`bg-pattern ${GeistSans.className} max-w-7xl mx-auto`}>
+      <body
+        className={`bg-pattern bg-fixed ${GeistSans.className} max-w-7xl mx-auto`}
+      >
         <Navbar />
         {children}
         <Analytics />
       </body>
     </html>
-  );
+  )
 }
