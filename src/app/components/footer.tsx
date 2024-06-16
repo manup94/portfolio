1097e94aa47b4238056037d45aff40c167b95d13
ManuelPerezPrado <manuel.perez@asafedigital.com>
2024-06-16 14:36:07 +0200

    improve footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="text-white md:p-20 p-10 md:text-end text-center">
-      <span className="font-semibold">Manuel Pérez - 2024</span>
+    <footer className="flex md:justify-end justify-center text-white p-8 md:text-end text-center">
+      <p className="font-semibold">Manuel Pérez - 2024</p>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
